072af3dfdf7c6f616214d2e1fc5e202878b742bd
yasir alshekahi <yaaserjoker1@gmail.com>
2025-09-13 22:01:54 +0300

    make some changes into db and create txt file in php folder to explain what to do in the php file

--- backend/db/userdb4.sql
+++ backend/db/userdb4.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 13, 2025 at 12:50 PM
+-- Generation Time: Sep 13, 2025 at 08:59 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,17 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `files` (
   `id` int(11) NOT NULL,
+  `file_name` varchar(254) NOT NULL,
   `the_file` varchar(255) NOT NULL,
   `teacher_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `files`
---
-
-INSERT INTO `files` (`id`, `the_file`, `teacher_id`) VALUES
-(1, '\"C:/ PC/file\"', 2),
-(2, '\"C:/PC/progfile\"', 2);
 
 -- --------------------------------------------------------
 
@@ -50,16 +43,8 @@ INSERT INTO `files` (`id`, `the_file`, `teacher_id`) VALUES
 CREATE TABLE `keywords` (
   `id` int(11) NOT NULL,
   `keyword` varchar(100) NOT NULL,
-  `teacher_id` int(11) NOT NULL
+  `file_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `keywords`
---
-
-INSERT INTO `keywords` (`id`, `keyword`, `teacher_id`) VALUES
-(1, 'what is python', 2),
-(2, 'how to use loops in python', 2);
 
 -- --------------------------------------------------------
 
@@ -74,15 +59,6 @@ CREATE TABLE `resources` (
   `keyword_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `resources`
---
-
-INSERT INTO `resources` (`id`, `title`, `link`, `keyword_id`) VALUES
-(1, 'what is python used for', 'https://www.youtube.com/watch?v=QoIRX37VZpo', 1),
-(2, 'loops in python', '\"youtube.com\"', 2),
-(3, 'while loop in python', '\"youtube.com/while\"', 2);
-
 -- --------------------------------------------------------
 
 --
@@ -96,14 +72,6 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `role` enum('teacher','student') NOT NULL DEFAULT 'student'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
-(1, 'علي احمد خالد', 'abc@gmail.com', '123abc', 'student'),
-(2, 'احمد خالد سعود', 'bcd@gmail.com', '123', 'teacher');
 
 --
 -- Indexes for dumped tables
@@ -121,7 +89,7 @@ ALTER TABLE `files`
 --
 ALTER TABLE `keywords`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `teacher_id` (`teacher_id`);
+  ADD KEY `file_id` (`file_id`);
 
 --
 -- Indexes for table `resources`
@@ -145,25 +113,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `keywords`
 --
 ALTER TABLE `keywords`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `resources`
 --
 ALTER TABLE `resources`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -179,7 +147,7 @@ ALTER TABLE `files`
 -- Constraints for table `keywords`
 --
 ALTER TABLE `keywords`
-  ADD CONSTRAINT `keywords_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `keywords_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `resources`
--- backend/python/api.py
+++ backend/python/api.py
@@ -10,23 +10,29 @@ CORS(app)
 @app.route("/API/resources", methods = ["POST"])
 def post_api():
     keyword = request.get_json() 
-    """{"keyword_id": 1, "keyword": "what is python"}"""
+    """{"id": 1, "keyword": "python"}"""
 
-
-
-db = mysql.connector.connect(
+    db = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="",
-    database="test"
+    database="userdb"
 )
 
+    gg = db.cursor()
 
-gg = db.cursor()
+    sql = "INSERT INTO resources (id, title, link, keyword_id) VALUES (NULL, %s, %s, %s)"
+    values = ("loops in python", "youtub.com", keyword["id"])
 
-gg.execute("SELECT * FROM person")
-ff = gg.fetchall()
-print(ff)
+    gg.execute(sql, values)
+    db.commit()
+
+    return jsonify({"status": "success", "message": "Resource added"}), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 
 
 
