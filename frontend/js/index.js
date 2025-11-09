c78bb8e20d77c4bc13893755b7d655c0f259553b
yasir alshekahi <yaaserjoker1@gmail.com>
2025-11-09 17:40:08 +0300

    finished from the session idea so now no need to log in every time u refresh the page and if u refresh the page the conntent will updeat with out asking u to log in again

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -31,10 +31,252 @@ async function loadSection(file, cssFile) {
   }
 }
 
+function showContent(data) {
+  const frame = document.getElementById("frame");
+  const userNameOnNaveBare = document.getElementById("username");
+  const teachers = data.teachers;
+  const user = data.user;
+
+  frame.innerHTML = "";
+
+  teachers.forEach((el) => {
+    const options =
+      el.files && el.files.length
+        ? el.files
+            .map(
+              (f) =>
+                `<option value="${f.file_id}" class="op">${f.file_name}</option>`
+            )
+            .join("")
+        : `<option value="">لا يوجد ملفات</option>`;
+    console.log(options);
+    frame.innerHTML += `
+<div class="card">
+  <div class="circle">
+    <svg viewBox="0 0 24 24">
+      <path
+        d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
+      />
+    </svg>
+  </div>
+  <h3>D. ${el.name}</h3>
+  <p>${el.email}</p>
+  <div class="btn-sel">
+    <select class="file-select" data-teacher-id="${el.id}">
+      <option value="">اختر ملف</option>
+      ${options}
+    </select>
+    <a href="#" class="contact-btn">View</a>
+  </div>
+</div>
+
+    `;
+  });
+
+  document.body.addEventListener("click", async (e) => {
+    if (e.target && e.target.id == "GoBack") {
+      document.getElementById("GoBack").addEventListener("click", async () => {
+        await loadSection(
+          "/frontend/html_pages/content.html",
+          "/frontend/css/content.css"
+        );
+
+        showContent(data);
+        document.body.style.direction = "rtl";
+      });
+    }
+
+    if (e.target && e.target.id === "submit-file") {
+      const file = document.getElementById("fileUpload");
+      const fileData = file.files[0];
+      const showFile = document.getElementById("showFilesSec");
+
+      if (showFile.innerHTML) {
+        const user = data.user;
+        const currentUserId = user.id;
+        const fileName = fileData.name.split(".")[0];
+        const filePath = fileData.name;
+        const fileType = fileData.type.split("/")[1];
+        const keywords = [];
+        const keywordElements = document.querySelectorAll(".Keywords-border p");
+
+        keywordElements.forEach((p) => {
+          keywords.push(p.textContent);
+        });
+
+        fetch("http://127.0.0.1:5000/API/uploadFile", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            currentuserid: currentUserId,
+            filename: fileName,
+            filepath: filePath,
+            filetype: fileType,
+            keywords: keywords,
+          }),
+        });
+      } else {
+        alert("plz choose a file");
+      }
+    }
+
+    if (e.target && e.target.id === "fileUpload") {
+      const file = document.getElementById("fileUpload");
+      const showFile = document.getElementById("showFilesSec");
+
+      file.addEventListener("change", () => {
+        const fileData = file.files[0];
+
+        let sizeText = "";
+        const sizeInBytes = fileData.size;
+
+        if (sizeInBytes < 1024 * 1024) {
+          const sizeInKB = (sizeInBytes / 1024).toFixed(2);
+          sizeText = `${sizeInKB} KB`;
+        } else {
+          const sizeInMB = (sizeInBytes / (1024 * 1024)).toFixed(2);
+          sizeText = `${sizeInMB} MB`;
+        }
+
+        showFile.innerHTML = `
+  <div class="file-border">
+    <i class="fa-solid fa-file"></i>
+
+    <div class="file-border-text-holder">
+      <h3 class="file-name-text">${fileData.name}</h3>
+      <p class="file-size-text">The size is ${sizeText} of 250 MB</p>
+    </div>
+    <div class="x-button" id="xbutton">
+      <button id="close-btn"><i class="fa-solid fa-x" id="clear-file"></i></button>
+    </div>
+  </div>
+  `;
+      });
+
+      showFile.addEventListener("click", (e) => {
+        if (e.target && e.target.id === "clear-file") {
+          showFile.innerHTML = "";
+          file.value = "";
+        }
+      });
+    }
+
+    if (e.target && e.target.id === "switch-btn") {
+      const fileContainer = document.getElementById("container");
+      const keywordContainer = document.getElementById("container-keyword");
+      const switchBtn = document.getElementById("switch-btn");
+
+      if (switchBtn.textContent === "Files section") {
+        fileContainer.style.display = "flex";
+        keywordContainer.style.display = "none";
+        switchBtn.textContent = "Keywords section";
+      } else {
+        fileContainer.style.display = "none";
+        keywordContainer.style.display = "flex";
+        switchBtn.textContent = "Files section";
+      }
+    }
+
+    if (e.target && e.target.id === "add-btn") {
+      const keywordContainer = document.getElementById("Keywords-container");
+      const keywordsEntry = document.getElementById("Keywords-entry");
+      const addBtn = document.getElementById("add-btn");
+      const clearAllBtn = document.getElementById("del-all-btn");
+
+      addBtn.addEventListener("click", () => {
+        keyword = keywordsEntry.value;
+        if (keyword) {
+          keywordContainer.innerHTML += `
+        <div class="Keywords-border">
+            <p>${keyword}</p>
+            <i class="fa-solid fa-x clear-all-icon" title="delet" id="del-keyword"></i>
+        </div>`;
+          keywordsEntry.value = "";
+        }
+      });
+
+      clearAllBtn.addEventListener("click", () => {
+        keywordContainer.innerHTML = "";
+        keywordsEntry.value = "";
+      });
+
+      keywordContainer.addEventListener("click", (e) => {
+        if (e.target && e.target.id === "del-keyword") {
+          e.target.closest(".Keywords-border").remove();
+        }
+      });
+    }
+  });
+
+  if (user.role === "teacher") {
+    const addFiles = document.getElementById("add-files");
+    addFiles.style.display = "felx";
+    addFiles.addEventListener("click", async () => {
+      await loadSection("/frontend/uploadfiles_ui/index.html", [
+        "/frontend/uploadfiles_ui/uploadFiles.css",
+        "/frontend/uploadfiles_ui/uploadKeywords.css",
+      ]);
+      document.body.style.direction = "ltr";
+    });
+  } else {
+    document.getElementById("add-files").style.display = "none";
+  }
+  userNameOnNaveBare.innerHTML += `${user.name}`;
+  userNameOnNaveBare.addEventListener("click", () => {
+    console.log("log out");
+  });
+  console.log(data);
+
+  document.querySelectorAll(".contact-btn").forEach((elements) => {
+    elements.addEventListener("click", () => {
+      const op = document.querySelectorAll(".op");
+      console.log(op);
+    });
+  });
+}
+
 let data = [];
 let sing = [];
+async function getDataUsingSessionStorage(data) {
+  const res = await fetch(
+    "http://localhost/web_project/backend/php/data_structure/get_data.php",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: data.email, password: data.password }),
+    }
+  );
+
+  data = await res.json();
+  if (data.error) {
+    console.log(data.error);
+    alert(data.error);
+  } else {
+    await loadSection(
+      "/frontend/html_pages/content.html",
+      "/frontend/css/content.css"
+    );
+    showContent(data);
+  }
+}
+
 (async () => {
-  await loadSection("/frontend/html_pages/home.html", "/frontend/css/home.css");
+  const email = sessionStorage.getItem("email");
+  const password = sessionStorage.getItem("password");
+  if (email && password) {
+    await loadSection(
+      "/frontend/html_pages/content.html",
+      "/frontend/css/content.css"
+    );
+
+    loginData = { email: email, password: password };
+    await getDataUsingSessionStorage(loginData);
+  } else {
+    await loadSection(
+      "/frontend/html_pages/home.html",
+      "/frontend/css/home.css"
+    );
+  }
 
   async function singInD(data) {
     const res = await fetch(
@@ -76,207 +318,13 @@ let sing = [];
       console.log(data.error);
       alert(data.error);
     } else {
-      const token = data.token
-      sessionStorage.setItem("token", token)
+      sessionStorage.setItem("email", loginData.email);
+      sessionStorage.setItem("password", loginData.password);
       await loadSection(
         "/frontend/html_pages/content.html",
         "/frontend/css/content.css"
       );
-      showContent();
-    }
-
-    function showContent() {
-      const frame = document.getElementById("frame");
-      const userNameOnNaveBare = document.getElementById("username");
-      const teachers = data.teachers;
-      const user = data.user;
-
-      frame.innerHTML = "";
-
-      teachers.forEach((el) => {
-        const options =
-          el.files && el.files.length
-            ? el.files
-                .map(
-                  (f) =>
-                    `<option value="${f.file_id}" class="op">${f.file_name}</option>`
-                )
-                .join("")
-            : `<option value="">لا يوجد ملفات</option>`;
-        console.log(options);
-        frame.innerHTML += `
-<div class="card">
-  <div class="circle">
-    <svg viewBox="0 0 24 24">
-      <path
-        d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
-      />
-    </svg>
-  </div>
-  <h3>D. ${el.name}</h3>
-  <p>${el.email}</p>
-  <div class="btn-sel">
-    <select class="file-select" data-teacher-id="${el.id}">
-      <option value="">اختر ملف</option>
-      ${options}
-    </select>
-    <a href="#" class="contact-btn">View</a>
-  </div>
-</div>
-
-    `;
-      });
-
-      document.body.addEventListener("click", async (e) => {
-        if (e.target && e.target.id === "submit-file") {
-          const file = document.getElementById("fileUpload");
-          const fileData = file.files[0];
-          const showFile = document.getElementById("showFilesSec");
-
-          if (showFile.innerHTML) {
-            const user = data.user;
-            const currentUserId = user.id;
-            const fileName = fileData.name.split(".")[0];
-            const filePath = fileData.name;
-            const fileType = fileData.type.split("/")[1];
-            const keywords = [];
-            const keywordElements =
-              document.querySelectorAll(".Keywords-border p");
-
-            keywordElements.forEach((p) => {
-              keywords.push(p.textContent);
-            });
-
-            fetch("http://127.0.0.1:5000/API/uploadFile", {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({
-                currentuserid: currentUserId,
-                filename: fileName,
-                filepath: filePath,
-                filetype: fileType,
-                keywords: keywords,
-              }),
-            });
-          } else {
-            alert("plz choose a file");
-          }
-        }
-
-        if (e.target && e.target.id === "fileUpload") {
-          const file = document.getElementById("fileUpload");
-          const showFile = document.getElementById("showFilesSec");
-
-          file.addEventListener("change", () => {
-            const fileData = file.files[0];
-
-            let sizeText = "";
-            const sizeInBytes = fileData.size;
-
-            if (sizeInBytes < 1024 * 1024) {
-              const sizeInKB = (sizeInBytes / 1024).toFixed(2);
-              sizeText = `${sizeInKB} KB`;
-            } else {
-              const sizeInMB = (sizeInBytes / (1024 * 1024)).toFixed(2);
-              sizeText = `${sizeInMB} MB`;
-            }
-
-            showFile.innerHTML = `
-  <div class="file-border">
-    <i class="fa-solid fa-file"></i>
-
-    <div class="file-border-text-holder">
-      <h3 class="file-name-text">${fileData.name}</h3>
-      <p class="file-size-text">The size is ${sizeText} of 250 MB</p>
-    </div>
-    <div class="x-button" id="xbutton">
-      <button id="close-btn"><i class="fa-solid fa-x" id="clear-file"></i></button>
-    </div>
-  </div>
-  `;
-          });
-
-          showFile.addEventListener("click", (e) => {
-            if (e.target && e.target.id === "clear-file") {
-              showFile.innerHTML = "";
-              file.value = "";
-            }
-          });
-        }
-
-        if (e.target && e.target.id === "switch-btn") {
-          const fileContainer = document.getElementById("container");
-          const keywordContainer = document.getElementById("container-keyword");
-          const switchBtn = document.getElementById("switch-btn");
-
-          if (switchBtn.textContent === "Files section") {
-            fileContainer.style.display = "flex";
-            keywordContainer.style.display = "none";
-            switchBtn.textContent = "Keywords section";
-          } else {
-            fileContainer.style.display = "none";
-            keywordContainer.style.display = "flex";
-            switchBtn.textContent = "Files section";
-          }
-        }
-
-        if (e.target && e.target.id === "add-btn") {
-          const keywordContainer =
-            document.getElementById("Keywords-container");
-          const keywordsEntry = document.getElementById("Keywords-entry");
-          const addBtn = document.getElementById("add-btn");
-          const clearAllBtn = document.getElementById("del-all-btn");
-
-          addBtn.addEventListener("click", () => {
-            keyword = keywordsEntry.value;
-            if (keyword) {
-              keywordContainer.innerHTML += `
-        <div class="Keywords-border">
-            <p>${keyword}</p>
-            <i class="fa-solid fa-x clear-all-icon" title="delet" id="del-keyword"></i>
-        </div>`;
-              keywordsEntry.value = "";
-            }
-          });
-
-          clearAllBtn.addEventListener("click", () => {
-            keywordContainer.innerHTML = "";
-            keywordsEntry.value = "";
-          });
-
-          keywordContainer.addEventListener("click", (e) => {
-            if (e.target && e.target.id === "del-keyword") {
-              e.target.closest(".Keywords-border").remove();
-            }
-          });
-        }
-      });
-
-      if (user.role === "teacher") {
-        const addFiles = document.getElementById("add-files");
-        addFiles.style.display = "felx";
-        addFiles.addEventListener("click", async () => {
-          await loadSection("/frontend/uploadfiles_ui/index.html", [
-            "/frontend/uploadfiles_ui/uploadFiles.css",
-            "/frontend/uploadfiles_ui/uploadKeywords.css",
-          ]);
-          document.body.style.direction = "ltr";
-        });
-      } else {
-        document.getElementById("add-files").style.display = "none";
-      }
-      userNameOnNaveBare.innerHTML += `${user.name}`;
-      userNameOnNaveBare.addEventListener("click", () => {
-        console.log("log out");
-      });
-      console.log(data);
-
-      document.querySelectorAll(".contact-btn").forEach((elements) => {
-        elements.addEventListener("click", () => {
-          const op = document.querySelectorAll(".op");
-          console.log(op);
-        });
-      });
+      showContent(data);
     }
 
     document.body.addEventListener("click", async (e) => {
@@ -285,21 +333,7 @@ let sing = [];
           "/frontend/html_pages/content.html",
           "/frontend/css/content.css"
         );
-        showContent();
-      }
-
-      if (e.target && e.target.id == "GoBack") {
-        document
-          .getElementById("GoBack")
-          .addEventListener("click", async () => {
-            await loadSection(
-              "/frontend/html_pages/content.html",
-              "/frontend/css/content.css"
-            );
-
-            showContent();
-            document.body.style.direction = "rtl";
-          });
+        showContent(data);
       }
     });
   }
@@ -316,6 +350,7 @@ let sing = [];
       const email = e.target.elements.email.value;
       const password = e.target.elements.password.value;
       loginData = { email: email, password: password };
+
       await logIn(loginData);
     });
   }
