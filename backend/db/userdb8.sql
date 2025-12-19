6cc683788ae8029e96193d885a1fa52bb45db1d8
yasir alshekahi <yaaserjoker1@gmail.com>
2025-12-19 16:30:49 +0300

    change the db

--- backend/db/userdb8.sql
+++ backend/db/userdb8.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2025 at 09:20 AM
+-- Generation Time: Dec 19, 2025 at 02:29 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,14 +34,6 @@ CREATE TABLE `files` (
   `file_type` varchar(254) NOT NULL,
   `teacher_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `files`
---
-
-INSERT INTO `files` (`id`, `file_name`, `file_path`, `file_type`, `teacher_id`) VALUES
-(2, 'js', 'js.pdf', '', 5),
-(3, 'pyhton', 'python.pdf', '', 5);
 
 -- --------------------------------------------------------
 
@@ -84,15 +76,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
-(5, 'ali Ahmed', 'ali@gmail.com', '$2y$10$EA/rhj5LVSU0h/uYY/PxgOC1CLCrjnYmSxcLtWB/.8MtJhpKSAIsu', 'teacher'),
-(18, 'refoabdo', 'refoabdo@gmail.com', '$2y$10$QD5C.kfYKbQtV.tTWuAW.eUIs7Z470BZW5/cVuvn.De1M0iTLYlhO', 'teacher'),
-(19, 'gggg', 'gg@gmail.com', '$2y$10$KXQwRmAvUjxvkI.fUmNQyenJhN/PdE7TM6aGsR4Ll8S6pmb62MsQ6', 'teacher');
-
---
 -- Indexes for dumped tables
 --
 
@@ -132,25 +115,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT for table `keywords`
 --
 ALTER TABLE `keywords`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=143;
 
 --
 -- AUTO_INCREMENT for table `resources`
 --
 ALTER TABLE `resources`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=968;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Constraints for dumped tables
